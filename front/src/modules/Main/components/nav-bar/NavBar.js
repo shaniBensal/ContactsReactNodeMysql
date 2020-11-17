ce309f93ba63bfcc3ffe84a6e1376061c6710d47
Shani <shano.bensal@mail.huji.ac.il>
2020-11-17 18:52:43 +0200

    edit-logo

--- front/src/modules/Main/components/nav-bar/NavBar.js
+++ front/src/modules/Main/components/nav-bar/NavBar.js
@@ -4,9 +4,12 @@ import { Navbar, Nav } from "react-bootstrap";
 const NavBar = () => {
   return (
     <Navbar bg="primary" variant="dark">
-      <Nav className="mr-auto">
-        <Nav.Link href="/">Home</Nav.Link>
+      <Nav>
+        <h4 className="text-white logo-font">My Organizer</h4>
       </Nav>
+      {/* <Nav className="mr-auto">
+        <Nav.Link href="/">Home</Nav.Link>
+      </Nav> */}
     </Navbar>
   );
 };
